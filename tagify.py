62f1f4ebfe94fd241f4d9616e60f88c5cf8bfba3
jen <jen@jens-macbook.local>
2009-11-11 16:03:36 -0500

    updated to handle proper linking and sentence return

--- tagify.py
+++ tagify.py
@@ -1,20 +1,23 @@
 import re
 
-def tagify(text):
-  return remove_stop_words(remove_ctrl_chars(text).split(' '))
-  
-def clean_word(word):
-  return re.compile('[,().;:"\'?!*&<>/\+={}`~]').sub('', word)
-  
-def remove_ctrl_chars(text):
-  return re.compile('[\r\n\t]').sub(' ', text)
+class Tagify():
+  def __init__(self,text,link_path=''):
+    text = re.compile('[\r\n\t]').sub(' ',text).split(' ')
+    self._get_tagged(text,link_path)
 
-def remove_stop_words(text):
-  tag_words = []
-  for word in text:
-    word = clean_word(word)
-    if len(word) > 2 and not bool(re.match('(^(th|wh|hi|sh|an|ha|wa)[adeisouy]$)|(^(th|wh|do|is|he)[aeionur][nste]$)|(^(wh|th|h|w)(ere))|(^(w|sh|c)(ould))|(but)|(how)|(very)|(really)', word, re.IGNORECASE)):
-      tag_words.append(word)
-        
-  return sorted(tag_words)
+  def _get_tagged(self,text,link_path):
+    tag_words = ''
+    stop_word = re.compile('(^(th|wh|hi|sh|an|ha|wa)[adeisouy]$)|(^(th|wh|do|is|he)[aeionur][nste]$)|(^(wh|th|h|w)(ere))|(^(w|sh|c)(ould))|(but)|(how)|(very)|(really)')
   
+    clean_word = re.compile('[\[\],().;:"\'?!*&<>/\+={}`~]')
+    link_path = re.compile('(\/)$').sub('',link_path)
+  
+    for word in text:
+      tag_word = clean_word.sub('',word).lower()
+    
+      if len(word) > 2 and not bool(stop_word.match(word,re.IGNORECASE)):
+        tag_words += '<a href="'+link_path+'/'+tag_word+'">'+word+'</a> '
+      else:
+        tag_words += word+' '
+        
+    print tag_words
