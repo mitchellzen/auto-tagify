c06fb690ff9ca129a5a9daa19550452ab13e313a
jen <jen@jens-macbook.local>
2009-11-11 21:42:28 -0500

    changing to min_word_length

--- auto_tagify.py
+++ auto_tagify.py
@@ -5,11 +5,11 @@ class auto_tagify():
   eol = re.compile('[\r\n\t]')
   clean_word = re.compile('[\[\],().;:"\'?!*&<>/\+={}`~]')
   clean_link = re.compile('(?<=^\/)\/+|\/+$')
-  min_length = 2
+  min_word_length = 3
   
   def __init__(self):
-    self.css_class = ''
-    self.link_path = ''
+    self.css = ''
+    self.link = ''
     self.text = ''
     
   def generate(self):
@@ -17,8 +17,8 @@ class auto_tagify():
 
     for word in self.eol.sub(' ',self.text).split(' '):
       tag_word = self.clean_word.sub('',word).lower()
-      if len(tag_word) > self.min_length and not self.stop_word.match(tag_word):
-        tag_words += '<a href="'+self.clean_link.sub('', self.link_path)+'/'+tag_word+'" class="'+self.clean_word.sub('',self.css_class)+'">'+word+'</a> '
+      if len(tag_word) >= self.min_word_length and not self.stop_word.match(tag_word):
+        tag_words += '<a href="'+self.clean_link.sub('', self.link)+'/'+tag_word+'" class="'+self.clean_word.sub('',self.css)+'">'+word+'</a> '
       else:
         tag_words += word+' '
 
@@ -29,7 +29,7 @@ class auto_tagify():
 
     for word in self.eol.sub(' ',self.text).split(' '):
       tag_word = self.clean_word.sub('',word).lower()
-      if len(tag_word) > self.min_length and not self.stop_word.match(word):
+      if len(tag_word) >= self.min_word_length and not self.stop_word.match(word):
         tag_words.append(tag_word)
  
     return tag_words
