ed8d1777089e67dffe0bab2101f71bd29566a6e1
jen <jen@jens-macbook.local>
2009-11-16 23:58:16 -0500

    added urllib to handle certain special characters

--- auto_tagify.py
+++ auto_tagify.py
@@ -1,9 +1,10 @@
 import re
+import urllib
 
 class AutoTagify():
-  stop_word = re.compile('(^(th|wh|hi|sh|an|ha|wa|it)[adeisouy]$)|(^(th|wh|do|is|he)[aeionur][nste]$)|(^(wh|th|h|w)(ere))|(^(w|sh|c)(ould))|(but)|(how)|(very)|(really)|(with)|(not)')
+  stop_word = re.compile('(^(th|wh|hi|sh|an|ha|wa|it)[adeisouy]$)|(^(th|wh|do|is|he)[aeionur][nstey]$)|(^(wh|th|h|w)(ere))|(^(w|sh|c)(ould))|(but)|(how)|(very)|(really)|(with)|(not)|(their)|(theyre)|(are)')
   eol = re.compile('[\r\n\t]')
-  clean_word = re.compile('[\[\],().;:"\'?!*&<>/\+={}`~#]')
+  clean_word = re.compile('[\[\],().:"\'?!*<>/\+={}`~]')
   clean_link = re.compile('(?<=^\/)\/+|\/+$')
   min_word_length = 3
   
@@ -18,7 +19,7 @@ class AutoTagify():
     for word in self.eol.sub(' ',self.text).split(' '):
       tag_word = self.clean_word.sub('',word).lower()
       if len(tag_word) >= self.min_word_length and not self.stop_word.match(tag_word):
-        tag_words += '<a href="'+self.clean_link.sub('', self.link)+'/'+tag_word+'" class="'+self.clean_word.sub('',self.css)+'">'+word+'</a> '
+        tag_words += '<a href="'+self.clean_link.sub('', self.link)+'/'+urllib.quote(tag_word)+'" class="'+self.clean_word.sub('',self.css)+'">'+word+'</a> '
       else:
         tag_words += word+' '
 
