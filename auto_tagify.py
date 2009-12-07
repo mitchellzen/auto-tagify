6bb15a00eb4fb78762190754950e1041e23b7276
jen <jen@jens-macbook.local>
2009-12-07 10:21:45 -0500

    added additional stop words and flag to return url encoded text/tags

--- auto_tagify.py
+++ auto_tagify.py
@@ -2,7 +2,7 @@ import re
 import urllib
 
 class AutoTagify():
-  stop_word = re.compile('(^(th|wh|hi|sh|an|ha|wa|it)[adeisouy]$)|(^(th|wh|do|is|he)[aeionur][nstey]$)|(^(wh|th|h|w)(ere))|(^(w|sh|c)(ould))|(but)|(how)|(very)|(really)|(with)|(not)|(their)|(theyre)|(are)')
+  stop_word = re.compile('(^(th|wh|hi|sh|an|ha|wa|it)[adeisouy]$)|(^(th|wh|do|is|he)[aeionur][nstey]$)|(^(wh|th|h|w)(ere))|(^(w|sh|c)(ould))|(but)|(how)|(very)|(really)|(with)|(not)|(their)|(theyre)|(are)|(for)|(because)')
   eol = re.compile('[\r\n\t]')
   clean_word = re.compile('[\[\],().:"\'?!*<>/\+={}`~]')
   clean_link = re.compile('(?<=^\/)\/+|\/+$')
@@ -13,11 +13,14 @@ class AutoTagify():
     self.link = ''
     self.text = ''
     
-  def generate(self):
+  def generate(self,strict=True):
     tag_words = ''
 
     for word in self.eol.sub(' ',self.text).split(' '):
-      tag_word = self.clean_word.sub('',word).lower()
+      if strict:
+        tag_word = self.clean_word.sub('',word.lower())
+      else:
+        tag_word = urllib.quote(word.lower())
       if len(tag_word) >= self.min_word_length and not self.stop_word.match(tag_word):
         tag_words += '<a href="'+self.clean_link.sub('', self.link)+'/'+urllib.quote(tag_word)+'" class="'+self.clean_word.sub('',self.css)+'">'+word+'</a> '
       else:
@@ -25,11 +28,14 @@ class AutoTagify():
 
     return tag_words
   
-  def tag_list(self):
+  def tag_list(self,strict=True):
     tag_words = []
-
+    
     for word in self.eol.sub(' ',self.text).split(' '):
-      tag_word = self.clean_word.sub('',word).lower()
+      if strict:
+        tag_word = self.clean_word.sub('',word.lower())
+      else:
+        tag_word = urllib.quote(word.lower())
       if len(tag_word) >= self.min_word_length and not self.stop_word.match(tag_word):
         tag_words.append(tag_word)
  
