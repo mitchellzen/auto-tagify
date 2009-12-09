dc0c2734c59c18884fcce9067a9b135959707547
jen <jen@jens-macbook.local>
2009-12-09 17:40:58 -0500

    fixed private methods

--- auto_tagify.py
+++ auto_tagify.py
@@ -9,7 +9,7 @@ class AutoTagify():
   smart_quotes_s = re.compile('(\xe2\x80\x98)|(\xe2\x80\x99)')
   smart_quotes_d = re.compile('(\xe2\x80\x9c)|(\xe2\x80\x9d)')
   clean_link = re.compile('(?<=^\/)\/+|\/+$')
-  stop_words = ['DT', 'IN', 'TO', 'VBD', 'VBG', 'VBN', 'VBZ', 'MD', 'RB']
+  stop_words = ['DT', 'IN', 'TO', 'VBD', 'VBD', 'VBG', 'VBN', 'VBZ', 'MD', 'RB']
   min_tag_length = 2
   lemma = WordNetLemmatizer()
   stem = PorterStemmer()
@@ -21,8 +21,8 @@ class AutoTagify():
     
   def generate(self,strict=True):
     tag_words = ''
-    for (word, word_type) in self._tokenize():
-      tag_word = self._cleaned(word,strict)
+    for (word, word_type) in self.__tokenize():
+      tag_word = self.__cleaned(word,strict)
       if len(tag_word) > self.min_tag_length and word_type not in self.stop_words:
         tag_words += '<a href="'+self.clean_link.sub('', self.link)+'/'+urllib.quote(tag_word)+'" class="'+self.clean_word.sub('',self.css)+'">'+word+'</a> '
       else:
@@ -31,15 +31,15 @@ class AutoTagify():
   
   def tag_list(self,strict=True):
     tag_words = []
-    for (word, word_type) in self._tokenize():
-      tag_word = self._cleaned(word,strict)
+    for (word, word_type) in self.__tokenize():
+      tag_word = self.__cleaned(word,strict)
       if len(tag_word) > self.min_tag_length and word_type not in self.stop_words: tag_words.append(tag_word)
     return tag_words
     
-  def _tokenize(self):
+  def __tokenize(self):
     return nltk.pos_tag(nltk.word_tokenize(self.text))
     
-  def _cleaned(self,word,strict):
+  def __cleaned(self,word,strict):
     lemmatized = self.stem.stem(self.lemma.lemmatize(self.clean_word.sub('',self.smart_quotes_s.sub('\'',self.smart_quotes_d.sub('"',word.lower())))))
     if strict:
       return lemmatized
