bca619fbebcdda577168d98d334c4eb5aa80b153
Edna Piranha <jen@ednapiranha.com>
2011-07-10 22:15:24 -0400

    added auto hyperlinks

--- auto_tagify.py
+++ auto_tagify.py
@@ -4,7 +4,7 @@ import nltk
 from nltk.stem.wordnet import WordNetLemmatizer
 
 class AutoTagify():
-  clean_word = re.compile('[\[\],().:;"\'?*%!*+=@$;#%{}`~\r\n\t]')
+  clean_word = re.compile('[\[\],().:;"\/\'?*%!*+=@$;#%{}`~\r\n\t]')
   smart_quotes_s = re.compile('(\xe2\x80\x98)|(\xe2\x80\x99)|(\&#8216;)|(\&#8217;)')
   smart_quotes_d = re.compile('(\xe2\x80\x9c)|(\xe2\x80\x9d)|(\&#8220;)|(\&#8221;)')
   long_dash = re.compile('(\&#8212;)')
@@ -21,14 +21,15 @@ class AutoTagify():
     self.link = ''
     self.text = ''
     
-  def generate(self,strict=True,html=False):
+  def generate(self,strict=True,html=True):
     tag_words = ''
     for (word, word_type) in self.__tokenize():
+      print word
       tag_word = self.__cleaned(word,strict)
       if len(tag_word) > self.min_tag_length and word_type not in self.stop_words:
-        if html and self.__is_hyperlink(tag_word):
-          tag_words += '<a href="'+tag_word+'" target="_blank">'+tag_word+'</a>'
-        else
+        if html and self.__is_hyperlink(word):
+          tag_words += '<a href="'+word+'" target="_blank">'+word+'</a>'
+        else:
           tag_words += '<a href="'+self.clean_link.sub('', self.link)+'/'+urllib.quote(tag_word)+'" class="'+self.clean_word.sub('',self.css)+'">'+self.__replace_special_chars(word)+'</a> '
       else:
         tag_words += word+' '
@@ -42,13 +43,16 @@ class AutoTagify():
     return tag_words
   
   def __is_hyperlink(self,word):
-    if link_pattern_a.match(word) or link_pattern_b.match(word):
-      return true
+    if self.link_pattern_a.match(word) or self.link_pattern_b.match(word):
+      return True
     else:
-      return false
+      return False
     
-  def __tokenize(self):
-    return nltk.pos_tag(nltk.word_tokenize(self.__clean_text()))
+  def __tokenize(self,html=True):
+    if html and self.__is_hyperlink(self.text):
+      return [(self.text, '')]
+    else:
+      return nltk.pos_tag(nltk.word_tokenize(self.__clean_text()))
     
   def __cleaned(self,word,strict):
     lemmatized = self.lemma.lemmatize(self.__clean_text(word))
@@ -57,7 +61,7 @@ class AutoTagify():
     else:
       return urllib.quote(lemmatized)
       
-  def __clean_text(self, word=''):
+  def __clean_text(self,word=''):
     if len(word) > self.min_tag_length:
       return self.clean_word.sub('',self.__replace_special_chars(word.lower()))
     else:
--- auto_tagifytests.py
+++ auto_tagifytests.py
@@ -2,31 +2,41 @@ import unittest
 from auto_tagify import AutoTagify
 
 class AutoTagifyTestCase(unittest.TestCase):
-		
-	def testTextNotEmpty(self):
-		"""
-		  Verify text returns content, if text is provided and not null
-		"""
-		a = AutoTagify()
-		a.text = 'This is a test'
-		a.css = "taggable"
-		self.assertEqual(a.generate(), 'This is a <a href="/test" class="taggable">test</a> ')
-		
-	def testTextEmpty(self):
-	  """
-	    Verify sending no text returns nothing
-	  """
-	  a = AutoTagify()
-	  self.assertEqual(a.generate(), '')
-	  
-	def testTagsNotEmpty(self):
-	  """
-	    Verify that tags are returned
-	  """
-	  a = AutoTagify()
-	  a.text = 'This is a test with other valid tags'
-	  test_array = ['test', 'other', 'valid', 'tag']
-	  self.assertEqual(a.tag_list(), test_array)
-	  
+    
+  def testTextNotEmpty(self):
+    """
+      Verify text returns content, if text is provided and not null
+    """
+    a = AutoTagify()
+    a.text = 'This is a test'
+    a.css = "taggable"
+    self.assertEqual(a.generate(), 'This is a <a href="/test" class="taggable">test</a> ')
+    
+  def testTextEmpty(self):
+    """
+      Verify sending no text returns nothing
+    """
+    a = AutoTagify()
+    self.assertEqual(a.generate(), '')
+    
+  def testTagsNotEmpty(self):
+    """
+      Verify that tags are returned
+    """
+    a = AutoTagify()
+    a.text = 'This is a test with other valid tags'
+    test_array = ['test', 'other', 'valid', 'tag']
+    self.assertEqual(a.tag_list(), test_array)
+    
+  
+  def testLinkIsGenerated(self):
+    """
+      Verify that a hyperlink to the posted link is generated
+    """
+    a = AutoTagify()
+    a.text = 'http://test.com'
+    test_link = '<a href="http://test.com" target="_blank">http://test.com</a>'
+    self.assertEqual(a.generate(), test_link)
+    
 if __name__ == "__main__":
     unittest.main()
