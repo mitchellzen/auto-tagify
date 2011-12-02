5429a12cc7b6dc30002eaccd7a88461eb8f632a8
Jen Fong-Adwent <jen@ednapiranha.com>
2011-12-01 23:07:11 -0500

    mega cleanup!

--- auto_tagify.py
+++ auto_tagify.py
@@ -1,56 +1,63 @@
 import re
 import urllib
+
 import nltk
+
 from nltk.stem.wordnet import WordNetLemmatizer
 
-class AutoTagify():
-  clean_word = re.compile('[\[\],().:;"\/\'?*%!*+=@$;#%{}`~\r\n\t]')
-  smart_quotes_s = re.compile('(\xe2\x80\x98)|(\xe2\x80\x99)|(\&#8216;)|(\&#8217;)')
-  smart_quotes_d = re.compile('(\xe2\x80\x9c)|(\xe2\x80\x9d)|(\&#8220;)|(\&#8221;)')
-  long_dash = re.compile('(\&#8212;)')
-  clean_link = re.compile('(?<=^\/)\/+|\/+$')
-  stop_words = ['DT', 'IN', 'TO', 'VBD', 'VBD', 'VBG', 'VBN', 'VBZ', 'MD', 'RB', 'CC', 'WDT']
-  min_tag_length = 2
-  lemma = WordNetLemmatizer()
-  
-  def __init__(self):
-    self.css = ''
-    self.link = ''
-    self.text = ''
-    
-  def generate(self,strict=True):
-    tag_words = ''
-    for (word, word_type) in self.__tokenize():
-      print word
-      tag_word = self.__cleaned(word,strict)
-      if len(tag_word) > self.min_tag_length and word_type not in self.stop_words:
-        tag_words += '<a href="'+self.clean_link.sub('', self.link)+'/'+urllib.quote(tag_word)+'" class="'+self.clean_word.sub('',self.css)+'">'+self.__replace_special_chars(word)+'</a> '
-      else:
-        tag_words += word+' '
-    return tag_words
-  
-  def tag_list(self,strict=True):
-    tag_words = []
-    for (word, word_type) in self.__tokenize():
-      tag_word = self.__cleaned(word,strict)
-      if len(tag_word) > self.min_tag_length and word_type not in self.stop_words: tag_words.append(tag_word)
-    return tag_words
+CLEAN_LINK = re.compile('(?<=^\/)\/+|\/+$')
+CLEAN_WORD = re.compile('[\[\],().:;"\/\'?*%!*+=@$;#%{}`~\r\n\t]')
+LONG_DASH = re.compile('(\&#8212;)')
+MIN_TAG_LENGTH = 2
+SMART_QUOTES_D = re.compile('(\xe2\x80\x9c)|(\xe2\x80\x9d)|(\&#8220;)|(\&#8221;)')
+SMART_QUOTES_S = re.compile('(\xe2\x80\x98)|(\xe2\x80\x99)|(\&#8216;)|(\&#8217;)')
+STOP_WORDS = ['DT', 'IN', 'TO', 'VBD', 'VBD', 'VBG', 'VBN', 'VBZ', 'MD', 'RB', 'CC', 'WDT']
 
-  def __tokenize(self):
-    return nltk.pos_tag(nltk.word_tokenize(self.__clean_text()))
+
+class AutoTagify():
+    lemma = WordNetLemmatizer()
     
-  def __cleaned(self,word,strict):
-    lemmatized = self.lemma.lemmatize(self.__clean_text(word))
-    if strict:
-      return lemmatized
-    else:
-      return urllib.quote(lemmatized)
-      
-  def __clean_text(self,word=''):
-    if len(word) > self.min_tag_length:
-      return self.clean_word.sub('',self.__replace_special_chars(word.lower()))
-    else:
-      return self.clean_word.sub('',self.__replace_special_chars(self.text))
-      
-  def __replace_special_chars(self, text):
-    return self.smart_quotes_s.sub('\'',self.smart_quotes_d.sub('"',self.long_dash.sub('-',text)))
+    def __init__(self):
+        self.css = ''
+        self.link = ''
+        self.text = ''
+        
+    def generate(self, strict=True):
+        tag_words = ''
+        for (word, word_type) in self._tokenize():
+            tag_word = self._cleaned(word,strict)
+            if len(tag_word) > MIN_TAG_LENGTH and word_type not in STOP_WORDS:
+                tag_words += '<a href="' + CLEAN_LINK.sub('', self.link) + '/'
+                tag_words += urllib.quote(tag_word) + '" class="'
+                tag_words += CLEAN_WORD.sub('', self.css) + '">'
+                tag_words += self._replace_special_chars(word)+'</a> '
+            else:
+                tag_words += word+' '
+        return tag_words
+    
+    def tag_list(self, strict=True):
+        tag_words = []
+        for (word, word_type) in self._tokenize():
+            tag_word = self._cleaned(word,strict)
+            if len(tag_word) > MIN_TAG_LENGTH and word_type not in STOP_WORDS:
+                tag_words.append(tag_word)
+        return tag_words
+
+    def _tokenize(self):
+        return nltk.pos_tag(nltk.word_tokenize(self._clean_text()))
+        
+    def _cleaned(self, word, strict):
+        lemmatized = self.lemma.lemmatize(self._clean_text(word))
+        if strict:
+            return lemmatized
+        else:
+            return urllib.quote(lemmatized)
+            
+    def _clean_text(self, word=''):
+        if len(word) > MIN_TAG_LENGTH:
+            return CLEAN_WORD.sub('', self._replace_special_chars(word.lower()))
+        else:
+            return CLEAN_WORD.sub('', self._replace_special_chars(self.text))
+            
+    def _replace_special_chars(self, text):
+        return SMART_QUOTES_S.sub('\'', SMART_QUOTES_D.sub('"', LONG_DASH.sub('-',text)))
--- auto_tagifytests.py
+++ auto_tagifytests.py
@@ -2,31 +2,31 @@ import unittest
 from auto_tagify import AutoTagify
 
 class AutoTagifyTestCase(unittest.TestCase):
-    
-  def testTextNotEmpty(self):
-    """
-      Verify text returns content, if text is provided and not null
-    """
-    a = AutoTagify()
-    a.text = 'This is a test'
-    a.css = "taggable"
-    self.assertEqual(a.generate(), 'This is a <a href="/test" class="taggable">test</a> ')
-    
-  def testTextEmpty(self):
-    """
-      Verify sending no text returns nothing
-    """
-    a = AutoTagify()
-    self.assertEqual(a.generate(), '')
-    
-  def testTagsNotEmpty(self):
-    """
-      Verify that tags are returned
-    """
-    a = AutoTagify()
-    a.text = 'This is a test with other valid tags'
-    test_array = ['test', 'other', 'valid', 'tag']
-    self.assertEqual(a.tag_list(), test_array)
-    
+        
+    def testTextNotEmpty(self):
+        """
+            Verify text returns content, if text is provided and not null
+        """
+        a = AutoTagify()
+        a.text = 'This is a test'
+        a.css = "taggable"
+        self.assertEqual(a.generate(), 'This is a <a href="/test" class="taggable">test</a> ')
+        
+    def testTextEmpty(self):
+        """
+            Verify sending no text returns nothing
+        """
+        a = AutoTagify()
+        self.assertEqual(a.generate(), '')
+        
+    def testTagsNotEmpty(self):
+        """
+            Verify that tags are returned
+        """
+        a = AutoTagify()
+        a.text = 'This is a test with other valid tags'
+        test_array = ['test', 'other', 'valid', 'tag']
+        self.assertEqual(a.tag_list(), test_array)
+        
 if __name__ == "__main__":
-    unittest.main()
+        unittest.main()
